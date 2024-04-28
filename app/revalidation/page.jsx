36df00106d551dc7e80ece77a512d2d879f0ee33
ahsanahmed45 <168276246+ahsanahmed45@users.noreply.github.com>
2024-04-28 20:44:45 +0500

    Update page.jsx

--- app/revalidation/page.jsx
+++ app/revalidation/page.jsx
@@ -48,5 +48,29 @@ export default async function Page() {
     );
 }
 
+async function RandomWikiArticle() {
+    const randomWiki = await fetch(randomWikiUrl, {
+        next: { revalidate: revalidateTTL, tags: [tagName] }
+    });
+
+    const content = await randomWiki.json();
+    let extract = content.extract;
+    if (extract.length > maxExtractLength) {
+        extract = extract.slice(0, extract.slice(0, maxExtractLength).lastIndexOf(' ')) + ' [...]';
+    }
+
+    return (
+        <div className="bg-white text-neutral-600 card my-6 max-w-2xl">
+            <div className="card-title text-3xl px-8 pt-8">{content.title}</div>
+            <div className="card-body py-4">
+                <div className="text-lg font-bold">{content.description}</div>
+                <p className="italic">{extract}</p>
+                <a target="_blank" rel="noopener noreferrer" href={content.content_urls.desktop.page}>
+                    From Wikipedia
+                </a>
+            </div>
+        </div>
+    );
+
 
 }
