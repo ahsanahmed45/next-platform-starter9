9e05834929f92c8621b6e914b319d292d604b601
ahsanahmed45 <168276246+ahsanahmed45@users.noreply.github.com>
2024-04-28 19:57:59 +0500

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,9 +4,12 @@ export function Footer() {
     return (
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="underline transition decoration-dashed text-primary underline-offset-8 hover:opacity-80">
-                    Next.js on Netlify
-                </Link>
+            
+                <section className="flex flex-col gap-4">
+                  
+                    <img src="https://thumbs.dreamstime.com/z/banner-inscription-flights-to-united-arab-banner-inscription-flights-to-united-arab-emirates-blue-107127486.jpg" />
+                </section>
+           
             </p>
         </footer>
     );
